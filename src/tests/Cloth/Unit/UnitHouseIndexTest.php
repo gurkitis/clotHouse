b4e016c54ee49027eb2a4da0c8432369af1687f3
Roberts Brants <robertsunbrants@gmail.com>
2022-01-13 10:57:36 +0200

    [unit-house-index] implemented unit-house-index unit tests

---
+++ src/tests/Cloth/Unit/UnitHouseIndexTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use App\Models\Cloth\ClothingUnit;
+
+class UnitHouseIndexTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setVariables();
+    }
+
+    /**
+     * Tests successful warehouse's clothing units data retrieval
+     *
+     * @return void
+     */
+    public function test_unit_house_index_200()
+    {
+        $call = $this->get('clothing/unit/warehouse/'
+            . '?org_id=' . $this->_organization['id']
+            . '&house_id=' . $this->_organizationWarehouse['id'],
+            ['Authorization' => 'Bearer ' . $this->_userBearer]
+        );
+
+        $call->assertResponseStatus(200);
+        $call->seeJsonEquals($this->_clothingUnits->toArray());
+    }
+}
